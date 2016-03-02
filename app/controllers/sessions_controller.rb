afa68c4bf060b475bf422a3c92af46fda7e66869
saichander <saichander.ramaram@gmail.com>
2016-03-02 13:37:09 +0530

    Add account activations

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,15 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_back_or user
+      if user.activated?
+        log_in user
+        redirect_back_or user
+      else
+        message = "account not activated"
+        message += "Check your email for the activation link"
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
       render 'new'
@@ -15,7 +22,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-  log_out
-  redirect_to root_url
+    log_out
+    redirect_to root_url
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,14 +11,52 @@
 #
 
 class User < ActiveRecord::Base
+  attr_accessor :activation_token
+  before_save {self.email = email.downcase}
+  before_create :create_activation_digest
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :name, presence: true, length: {maximum:255}
   validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_senstive: false }
+    format: { with: VALID_EMAIL_REGEX },
+    uniqueness: { case_senstive: false }
   validates :password, presence: true, length: { minimum: 6}, allow_nil: true
 
   has_secure_password
-  before_save {self.email = email.downcase}
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+  #returns the hash digest of given string
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+      BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Returns a random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  #creates and assigns the activation token and digest
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
+
+  # Activates an account.
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
 end
